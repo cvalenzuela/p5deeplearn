919d5f478e185b0b7e7a28e13032c3cb1d6a17a6
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-09-25 18:58:34 -0400

    clean mnist

--- examples/es6/mnist/predict.js
+++ examples/es6/mnist/predict.js
@@ -4,43 +4,40 @@ Predict a drawn number.
 
 import { CheckpointLoader, NDArrayMathGPU, Session, Graph, Array1D } from 'deeplearn';
 
-let _a, sess, input, probs, math;
+let math, graphModel, input, probs, session;
 
-let predict = (data, resultTag ) => {
-  let reader = new CheckpointLoader('.');
+let reader = new CheckpointLoader('model/');
+reader.getAllVariables().then((checkpoints) => {
+  math = new NDArrayMathGPU();
+  graphModel = buildModelGraph(checkpoints);
+  input = graphModel[0];
+  probs = graphModel[1];
+  session = new Session(input.node.graph, math);
+});
 
-  reader.getAllVariables().then((vars) => {
-    math = new NDArrayMathGPU();
-    _a = buildModelGraphAPI(data, vars);
-
-    input = _a[0];
-    probs = _a[1];
-    sess = new Session(input.node.graph, math);
-
-    math.scope(() => {
-      let inputData = Array1D.new(data);
-      let probsVal = sess.eval(probs, [{
-        tensor: input,
-        data: inputData
-      }]);
-      console.log('Prediction: ' + probsVal.get());
-      resultTag.html('Prediction: ' + probsVal.get());
-      sess.dispose();
-    });
+let predict = (data, resultTag) => {
+  math.scope((keep, track) => {
+    let inputData = track(Array1D.new(data));
+    let probsVal = session.eval(probs, [{
+      tensor: input,
+      data: inputData
+    }]);
+    console.log('Prediction: ' + probsVal.get());
+    resultTag.html('Prediction: ' + probsVal.get());
   });
 };
 
-let buildModelGraphAPI = (data, vars) => {
+let buildModelGraph = (checkpoints) => {
   let g = new Graph();
   let input = g.placeholder('input', [784]);
-  let hidden1W = g.constant(vars['hidden1/weights']);
-  let hidden1B = g.constant(vars['hidden1/biases']);
+  let hidden1W = g.constant(checkpoints['hidden1/weights']);
+  let hidden1B = g.constant(checkpoints['hidden1/biases']);
   let hidden1 = g.relu(g.add(g.matmul(input, hidden1W), hidden1B));
-  let hidden2W = g.constant(vars['hidden2/weights']);
-  let hidden2B = g.constant(vars['hidden2/biases']);
+  let hidden2W = g.constant(checkpoints['hidden2/weights']);
+  let hidden2B = g.constant(checkpoints['hidden2/biases']);
   let hidden2 = g.relu(g.add(g.matmul(hidden1, hidden2W), hidden2B));
-  let softmaxW = g.constant(vars['softmax_linear/weights']);
-  let softmaxB = g.constant(vars['softmax_linear/biases']);
+  let softmaxW = g.constant(checkpoints['softmax_linear/weights']);
+  let softmaxB = g.constant(checkpoints['softmax_linear/biases']);
   let logits = g.add(g.matmul(hidden2, softmaxW), softmaxB);
   return [input, g.argmax(logits)];
 };
--- examples/es6/rnn/lstm.js
+++ examples/es6/rnn/lstm.js
@@ -2,35 +2,70 @@
 LSTM/RNN Generator
 */
 
-import {Array1D, NDArrayMathGPU, Scalar} from 'deeplearn';
+import { Array3D, NDArrayMathGPU, CheckpointLoader, Session } from 'deeplearn';
 import { hamlet } from './hamlet';
+
+let math, _a;
+
+let getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 let text = hamlet.toLowerCase();
 let maxlen = 40;
-let chars = new Set(Array.from(text).sort()); // \n for ↵ ?
+let chars = Array.from(new Set(Array.from(text))).sort(); // \n for ↵ ?
+let char_indices = chars.reduce((acc, cur, i) => {
+  acc[cur] = i;
+  return acc;
+}, {});
+let indices_char = chars.reduce((acc, cur, i) => {
+  acc[i] = cur;
+  return acc;
+}, {});
 
-let char_indices = {};
-for (let item of chars) {
-  console.log(item);
-}
-// let char_indices = chars.reduce(function(acc, cur, i) {
-//   acc[i] = cur;
-//   return acc;
-// }, {});
+let start_index = getRandomInt(0, text.length - maxlen - 1);
+let diversity = 0.5;
+let generated = '';
+let sentence = text.substring(start_index, start_index + maxlen);
+generated += sentence;
 
-let indices_char;
+let reader = new CheckpointLoader('.');
 
-// const math = new NDArrayMathGPU();
-// const a = Array1D.new([1, 2, 3]);
-// const b = Scalar.new(2);
-// math.scope(() => {
-//   const result = math.add(a, b);
-//   console.log(result.getValues());  // Float32Array([3, 4, 5])
-// });
+reader.getAllVariables().then((vars) => {
+  math = new NDArrayMathGPU();
+  _a = buildModelGraphAPI(data, vars);
 
+  input = _a[0];
+  probs = _a[1];
+  sess = new Session(input.node.graph, math);
+
+  math.scope(() => {
+    let inputData = track(Array1D.new(data));
+    let probsVal = sess.eval(probs, [{
+      tensor: input,
+      data: inputData
+    }]);
+    console.log('Prediction: ' + probsVal.get());
+    resultTag.html('Prediction: ' + probsVal.get());
+    sess.dispose();
+  });
+});
 
 let lstm = () =>  {
-  
+  for (let i = 0; i < 50; i++) {
+    let x = Array3D.zeros([1, maxlen, chars.length]);
+    Array.from(sentence).forEach((char, i) => {
+      x[0, i, char_indices[char]] = 1.
+    })
+    let preds = sess.eval(probs, [{
+      tensor: input,
+      data: inputData
+    }]);
+  }
 }
+
+lstm();
 
 export { lstm }
